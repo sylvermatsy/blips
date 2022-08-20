f61d6e80eadfa4e01400ef639ae64771fe5c3c1e
SylverMATSY <103262166+sylvermatsy@users.noreply.github.com>
2022-08-20 15:48:24 +0200

    Bug Fix

---
+++ sy_blips/config.lua
@@ -0,0 +1,19 @@
+Map = {
+  -- Exemple --> {name="~b~Publique~s~ | ~y~Place Cubique", id=765, color=77, weight= 0.6, ZoneBlip = true, Position = vector3(196.5047, -935.833, 75.62717)},
+    } 
+
+--[[
+Docs Utils =
+
+Blips ID : https://docs.fivem.net/docs/game-references/blips/
+Blips Color : https://docs.fivem.net/docs/game-references/blips/#blip-colors
+Blips Weight = Taille entre 0.0 et 1.0
+Position = vector3 vMenu / MenuAdmin [ vector3(x, y, z) ]
+
+
+-------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
+--------------------------- Created By "Sylver MATSY#7445 ---------------------------------------
+--------------------------- https://discord.gg/HKk8MQMaMW ---------------------------------------
+-------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
